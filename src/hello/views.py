9650dc2b28987f497193bab408e248ecda04f6db
Ritikesh Gupta <ritikesh1999@gmail.com>
2021-01-24 00:36:50 +0530

    API Test

--- src/hello/views.py
+++ src/hello/views.py
@@ -1,7 +1,15 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, jsonify, request
+from flask_cors import cross_origin
+
 hello_blueprint = Blueprint('hello',__name__)
+test_blueprint = Blueprint("test", __name__)
 
 @hello_blueprint.route('/')
 @hello_blueprint.route('/hello')
 def index():
 	return render_template('index.html')
+	
+@test_blueprint.route("/test", methods=["GET", "POST"])
+@cross_origin()
+def test():
+    return jsonify({"success": True, "type": request.method})
